cdc9faeb67a2b1d500379f136a106f03b9162618
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-16 12:16:15 -0300

    se termino con la logica del delete

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Activity } from "../types";
 import { categories } from "../data/categories";
 import { Dispatch, useMemo } from "react";
-import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityListProps = {
@@ -52,6 +52,17 @@ export default function ActivityList({
               }
             >
               <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+            </button>
+
+            <button
+              onClick={() =>
+                dispatch({
+                  type: "delete-activity",
+                  payload: { id: activity.id },
+                })
+              }
+            >
+              <XCircleIcon className="h-8 w-8 text-red-500" />
             </button>
           </div>
         </div>
